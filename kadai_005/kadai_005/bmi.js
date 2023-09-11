a411e3ebebec8586c9bcfb2812fb9c4f70ad2083
renatkyldn <142720203+renatkyldn@users.noreply.github.com>
2023-09-11 04:44:35 +0100

    kadai_005 JavaScript 再提出用

---
+++ kadai_005/kadai_005/bmi.js
@@ -0,0 +1,16 @@
+// 変数の宣言
+let userNumber;
+
+userNumber = 68;
+
+let userNumber2;
+
+userNumber2 = 1.7;
+
+let userNumber3;
+
+userNumber3 = userNumber/(userNumber2*userNumber2)
+
+console.log(userNumber3);
+
+
